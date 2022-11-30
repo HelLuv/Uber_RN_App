6a2b1e9a6985214f8a10ece1d75641a0118e3071
HelLuv <works.person@gmail.com>
2022-11-30 23:01:11 +0300

    setup navigation & HomeScreen

--- App.tsx
+++ App.tsx
@@ -1,18 +1,16 @@
 import {StatusBar} from 'expo-status-bar';
-import {Text, View} from 'react-native';
 import {Provider} from "react-redux";
 
 import store from "./src/store";
+import Navigation from "./src/navigation";
 
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto"/>
-      </View>
+      <Navigation/>
+      <StatusBar style="auto"/>
     </Provider>
   );
-}
+};
 
